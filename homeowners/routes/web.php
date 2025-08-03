b17d5166958aface1d6963b50bf9511fba69d01a
Joel Rutter <joel.rutter@sykescottages.co.uk>
2025-08-03 12:07:02 +0100

    add route to get data into controller

--- homeowners/routes/web.php
+++ homeowners/routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::post(
+    '/homeowners', [App\Http\Controllers\HomeownersController::class, 'store']
+)->name('homeowners.store');
